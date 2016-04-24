2e0a04162592cc34554432f31c1bae004a35ace1
kailevy <kai0levy@gmail.com>
2016-04-24 16:53:07 -0400

    Add bigger box and custom signal ability

--- moduviz.py
+++ moduviz.py
@@ -7,6 +7,12 @@ from kivy.garden.graph import Graph, MeshLinePlot, MeshStemPlot, SmoothLinePlot
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+class BiggerBox(BoxLayout):
+    def __init__(self,w1,w2):
+        super(BiggerBox, self).__init__(orientation='horizontal')
+        self.add_widget(w1)
+        self.add_widget(w2)
+
 class Grapher(BoxLayout):
     def __init__(self, n=3, xmin=0, xmax=10, ymin=-1, ymax=1, xlabel='Time'):
         """
@@ -51,28 +57,25 @@ class Grapher(BoxLayout):
         self.plots[p].points = signal
 
 class Main(App):
-    def __init__(self,Ts,tf,signal):
+    def __init__(self,Ts,signal):
         """
         Ts: sample time step
-        tf: time or frequency- time=0, frequency=1
         signal: modulation signals
         """
         super(Main, self).__init__()
         self.Ts = Ts
-        self.tf = tf
         self.signal = signal
 
     def build(self):
         time = Grapher()
         fourier = Grapher(3,-0.5/self.Ts,0.5/self.Ts, 0, 1, 'Freq')
-        graphs = [time, fourier]
         for i in range(3):
             select = [(self.signal[0][x], self.signal[i+1][x]) for x in range(len(self.signal[0]))]
             time.graph_points(i,select)
             transform = view_freq(self.signal[i+1], self.Ts)
             fourier.graph_points(i, [(transform[0][x], transform[1][x]) for x in range(len(transform[0]))])
 
-        return graphs[self.tf]
+        return BiggerBox(time,fourier)
 
 
 def square(t):
@@ -101,7 +104,7 @@ def plot_mods(times, data,carrier,modulated):
     plt.ylabel('Amplitude')
     plt.show()
 
-def fm_test(data_fn, time, Ts, wc, kf, A):
+def fm_test(time, Ts, wc, kf, A, signal=None, data_fn=None):
     """
     data_fn: function for data signal
     time: length of signal
@@ -110,30 +113,37 @@ def fm_test(data_fn, time, Ts, wc, kf, A):
     kf: hertz/volt variation from carrier frequency
     A: amplitude of output signal
     """
-    fn = np.vectorize(data_fn)
     times = np.arange(0,time,Ts)
     carrier = np.sin(np.multiply(times,2*np.pi*wc))
     integral = [0]
+    if data_fn:
+        fn = np.vectorize(data_fn)
+        signal = fn(times)
+        # for t in times:
+        #     integral.append(integral[-1] + Ts*fn(t))
+    for i,t in enumerate(times):
+        integral.append(integral[-1] + Ts*signal[i])
 
-    for t in times:
-        integral.append(integral[-1] + Ts*fn(t))
     fm = np.multiply(A,np.cos(np.multiply(np.add(np.multiply(times,wc),np.multiply(integral[1:],kf)),2*np.pi)))
 
-    return times, fn(times), carrier, fm
+    return times, signal, carrier, fm
 
-def am_test(data_fn, time, Ts, wc):
+def am_test(time, Ts, wc, signal=None, data_fn=None):
     """
     data_fn: function for data signal (must take array)
     time: length of signal
     Ts: discrete time sample interval
     wc: carrier frequency
     """
-    fn = np.vectorize(data_fn)
     times = np.arange(0,time,Ts)
     carrier = np.cos(np.multiply(times,2*np.pi*wc))
-    am = np.multiply(fn(times),carrier)
+    if data_fn:
+        fn = np.vectorize(data_fn)
+        signal = fn(times)
 
-    return times, fn(times), carrier, am
+    am = np.multiply(signal, carrier)
+
+    return times, signal, carrier, am
 
 def view_freq(signal, Ts):
     """
@@ -148,7 +158,7 @@ def view_freq(signal, Ts):
 
 
 if __name__ == '__main__':
-    Main(0.01, 1, am_test(np.sin, 10, 0.01, 3)).run()
-    # view_freq(am_test(np.sin, 10, 0.01, 3)[2], 0.01)
-    # plot_mods(*am_test(np.sin, 10, 0.001, 3))
-    # plot_mods(*fm_test(np.sin, 10, 0.001, 3, 2, 1))
+    times = np.arange(0,10,0.01)
+    signal = np.sin(times)
+    Main(0.01, am_test(10, 0.01, 3, signal=signal)).run()
+    # Main(0.01, fm_test(10, 0.01, 3, 2, 1, signal=signal)).run()

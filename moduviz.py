6b35ae78a10939a6c6b3657840edd3107e3f8218
kailevy <kai0levy@gmail.com>
2016-04-24 16:13:21 -0400

    Add basic kivy visualization

--- moduviz.py
+++ moduviz.py
@@ -2,9 +2,61 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+import kivy
+from kivy.garden.graph import Graph, MeshLinePlot, MeshStemPlot, SmoothLinePlot
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class Grapher(BoxLayout):
+    def __init__(self, n=3, xmin=0, xmax=10, ymin=-1, ymax=1, xlabel='Time'):
+        super(Grapher, self).__init__(orientation='vertical')
+        self.graph = []
+        self.plots = []
+        self.plots.append(MeshStemPlot(color=[1,0,0,1]))
+        self.plots.append(MeshLinePlot(color=[0,1,0,1]))
+        self.plots.append(MeshLinePlot(color=[0,0,1,1]))
+        self.reset_plots()
+        for i in range(n):
+            self.graph.append(Graph(xlabel=xlabel, ylabel='Amp', x_ticks_minor=5,
+            x_ticks_major=25, y_ticks_major=1,
+            y_grid_label=True, x_grid_label=True, padding=5,
+            x_grid=True, y_grid=True, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax))
+            self.graph[i].add_plot(self.plots[i])
+            self.add_widget(self.graph[i])
+
+    def reset_plots(self):
+        for plot in self.plots:
+            plot.points = [(0,0)]
+
+    def graph_points(self,p,signal):
+        self.plots[p].points = signal
+
+    def get_graphs(self):
+        return self.graph
+
+class Main(App):
+
+    def build(self):
+        Ts = 0.01
+        time = Grapher()
+        fourier = Grapher(3,-0.5/Ts,0.5/Ts, 0, 1, 'Freq')
+        graphs = [time, fourier]
+        signal = np.array(fm_test(np.sin, 10, Ts, 3, 2, 1))
+        # sel = [0,3]
+        # signal = list(signal[sel])
+        for i in range(3):
+            select = [(signal[0][x], signal[i+1][x]) for x in range(len(signal[0]))]
+            time.graph_points(i,select)
+            transform = view_freq(signal[i+1], Ts)
+            fourier.graph_points(i, [(transform[0][x], transform[1][x]) for x in range(len(transform[0]))])
+
+        return graphs[1]
+
+
 def square(t):
     freq = 2
     return 2*(int(freq*t)%2)-1
+
 
 def plot_mods(times, data,carrier,modulated):
     plt.subplot(3,1,1)
@@ -61,20 +113,22 @@ def am_test(data_fn, time, step, wc):
 def view_freq(signal, step):
     """
     signal: signal vector to be plotted
-    step: diiscrete time sample interval
+    step: discrete time sample interval
     """
     n = len(signal) # length of the signal
     frq = np.fft.fftfreq(signal.size, step)
 
     Y = np.fft.fft(signal)/n # fft computing and normalization
 
-    plt.stem(frq,np.abs(Y),'r') # plotting the spectrum
-    plt.xlabel('Freq (Hz)')
-    plt.ylabel('|Y(freq)|')
-    plt.show()
+    # plt.stem(frq,np.abs(Y),'r') # plotting the spectrum
+    # plt.xlabel('Freq (Hz)')
+    # plt.ylabel('|Y(freq)|')
+    # plt.show()
+    return frq, np.abs(Y)
 
 
 if __name__ == '__main__':
-    view_freq(am_test(np.sin, 10, 0.01, 3)[2], 0.01)
+    Main().run()
+    # view_freq(am_test(np.sin, 10, 0.01, 3)[2], 0.01)
     # plot_mods(*am_test(np.sin, 10, 0.001, 3))
     # plot_mods(*fm_test(np.sin, 10, 0.001, 3, 2, 1))

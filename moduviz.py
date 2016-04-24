77a5c4b4b87ac7bcbd4c9e2370d60e6260c8ad4a
kailevy <kai0levy@gmail.com>
2016-04-24 16:25:15 -0400

    Add more modularity and documentation

--- moduviz.py
+++ moduviz.py
@@ -9,12 +9,25 @@ from kivy.uix.boxlayout import BoxLayout
 
 class Grapher(BoxLayout):
     def __init__(self, n=3, xmin=0, xmax=10, ymin=-1, ymax=1, xlabel='Time'):
+        """
+        n: number of graphs
+        xmin: minimum x
+        xmax: maximum x
+        ymin: minimum y
+        ymax: maximum y
+        xlabel: label for x axis (time of freq)
+        """
         super(Grapher, self).__init__(orientation='vertical')
         self.graph = []
         self.plots = []
-        self.plots.append(MeshStemPlot(color=[1,0,0,1]))
-        self.plots.append(MeshLinePlot(color=[0,1,0,1]))
-        self.plots.append(MeshLinePlot(color=[0,0,1,1]))
+        if xlabel == 'Time':
+            self.plots.append(MeshLinePlot(color=[1,0,0,1]))
+            self.plots.append(MeshLinePlot(color=[0,1,0,1]))
+            self.plots.append(MeshLinePlot(color=[0,0,1,1]))
+        else:
+            self.plots.append(MeshStemPlot(color=[1,0,0,1]))
+            self.plots.append(MeshStemPlot(color=[0,1,0,1]))
+            self.plots.append(MeshStemPlot(color=[0,0,1,1]))
         self.reset_plots()
         for i in range(n):
             self.graph.append(Graph(xlabel=xlabel, ylabel='Amp', x_ticks_minor=5,
@@ -25,32 +38,41 @@ class Grapher(BoxLayout):
             self.add_widget(self.graph[i])
 
     def reset_plots(self):
+        """
+        resets plots
+        """
         for plot in self.plots:
             plot.points = [(0,0)]
 
     def graph_points(self,p,signal):
+        """
+        adds points from signal to index p
+        """
         self.plots[p].points = signal
 
-    def get_graphs(self):
-        return self.graph
-
 class Main(App):
+    def __init__(self,Ts,tf,signal):
+        """
+        Ts: sample time step
+        tf: time or frequency- time=0, frequency=1
+        signal: modulation signals
+        """
+        super(Main, self).__init__()
+        self.Ts = Ts
+        self.tf = tf
+        self.signal = signal
 
     def build(self):
-        Ts = 0.01
         time = Grapher()
-        fourier = Grapher(3,-0.5/Ts,0.5/Ts, 0, 1, 'Freq')
+        fourier = Grapher(3,-0.5/self.Ts,0.5/self.Ts, 0, 1, 'Freq')
         graphs = [time, fourier]
-        signal = np.array(fm_test(np.sin, 10, Ts, 3, 2, 1))
-        # sel = [0,3]
-        # signal = list(signal[sel])
         for i in range(3):
-            select = [(signal[0][x], signal[i+1][x]) for x in range(len(signal[0]))]
+            select = [(self.signal[0][x], self.signal[i+1][x]) for x in range(len(self.signal[0]))]
             time.graph_points(i,select)
-            transform = view_freq(signal[i+1], Ts)
+            transform = view_freq(self.signal[i+1], self.Ts)
             fourier.graph_points(i, [(transform[0][x], transform[1][x]) for x in range(len(transform[0]))])
 
-        return graphs[1]
+        return graphs[self.tf]
 
 
 def square(t):
@@ -59,6 +81,9 @@ def square(t):
 
 
 def plot_mods(times, data,carrier,modulated):
+    """
+    uses matplotlib to plot the modulating signals
+    """
     plt.subplot(3,1,1)
     plt.plot(times, data)
     plt.title('Data Signal')
@@ -76,59 +101,54 @@ def plot_mods(times, data,carrier,modulated):
     plt.ylabel('Amplitude')
     plt.show()
 
-def fm_test(data_fn, time, step, wc, kf, A):
+def fm_test(data_fn, time, Ts, wc, kf, A):
     """
     data_fn: function for data signal
     time: length of signal
-    step: discrete time sample interval
+    Ts: discrete time sample interval
     wc: carrier frequency
     kf: hertz/volt variation from carrier frequency
     A: amplitude of output signal
     """
     fn = np.vectorize(data_fn)
-    times = np.arange(0,time,step)
+    times = np.arange(0,time,Ts)
     carrier = np.sin(np.multiply(times,2*np.pi*wc))
     integral = [0]
 
     for t in times:
-        integral.append(integral[-1] + step*fn(t))
+        integral.append(integral[-1] + Ts*fn(t))
     fm = np.multiply(A,np.cos(np.multiply(np.add(np.multiply(times,wc),np.multiply(integral[1:],kf)),2*np.pi)))
 
     return times, fn(times), carrier, fm
 
-def am_test(data_fn, time, step, wc):
+def am_test(data_fn, time, Ts, wc):
     """
     data_fn: function for data signal (must take array)
     time: length of signal
-    step: discrete time sample interval
+    Ts: discrete time sample interval
     wc: carrier frequency
     """
     fn = np.vectorize(data_fn)
-    times = np.arange(0,time,step)
+    times = np.arange(0,time,Ts)
     carrier = np.cos(np.multiply(times,2*np.pi*wc))
     am = np.multiply(fn(times),carrier)
 
     return times, fn(times), carrier, am
 
-def view_freq(signal, step):
+def view_freq(signal, Ts):
     """
     signal: signal vector to be plotted
-    step: discrete time sample interval
+    Ts: discrete time sample interval
     """
     n = len(signal) # length of the signal
-    frq = np.fft.fftfreq(signal.size, step)
+    frq = np.fft.fftfreq(signal.size, Ts)
 
     Y = np.fft.fft(signal)/n # fft computing and normalization
-
-    # plt.stem(frq,np.abs(Y),'r') # plotting the spectrum
-    # plt.xlabel('Freq (Hz)')
-    # plt.ylabel('|Y(freq)|')
-    # plt.show()
     return frq, np.abs(Y)
 
 
 if __name__ == '__main__':
-    Main().run()
+    Main(0.01, 1, am_test(np.sin, 10, 0.01, 3)).run()
     # view_freq(am_test(np.sin, 10, 0.01, 3)[2], 0.01)
     # plot_mods(*am_test(np.sin, 10, 0.001, 3))
     # plot_mods(*fm_test(np.sin, 10, 0.001, 3, 2, 1))
